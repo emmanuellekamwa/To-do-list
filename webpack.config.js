8e531b00d24b24aa2bb1f50feaa5c016cadc1d4e
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-08-23 13:42:34 +0100

    Finish setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+       rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+         },
+        ],
+      },
+
+  plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Output Management',
+        }),
+      ],
+};
