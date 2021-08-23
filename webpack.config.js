05ba5d258329fab0819a3bf346c74cbdb91c021a
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-08-23 19:24:50 +0100

    Correct linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'To-do-list',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+  },
   output: {
-    filename: 'index.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
 
   module: {
-       rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-         },
-        ],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-
-  plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Output Management',
-        }),
-      ],
+    ],
+  },
 };
