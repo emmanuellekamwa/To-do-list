f040bec56703c6e8eefced5d372e0a446dea9bb1
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-08-23 14:01:18 +0100

    As

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './style.css';
+
+const list = [
+    {
+        description: 'Say morning prayers',
+        completed: false,
+        index: 3,
+    }, 
+    {
+        description: 'Do the laundry',
+        completed: false,
+        index: 2,
+    },
+    {
+        description: 'Pick children from school',
+        completed: true,
+        index: 1,
+    },
+]
